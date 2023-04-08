f52226bacf188245a59bb60dda587cec424d8964
vekxze <vekxze@proton.me>
2023-04-07 23:06:23 -0400

    adding window props constants

--- src/utils/constants.h
+++ src/utils/constants.h
@@ -1,9 +1,14 @@
 #pragma once
 
+/* WINDOW CONSTANTS */
+constexpr int WINDOW_WIDTH = 1280;
+constexpr int WINDOW_HEIGHT = 720;
+constexpr int WINDOW_TARGET_FPS = 60;
+
 /* SPLASH SCREEN CONSTANTS */
 constexpr int SPLASH_SCREEN_DURATION_MS = 2000;
 
-/* GAME CONSTANTS */
+/* INGAME CONSTANTS */
 constexpr int GAME_MIN_PLAYER = 1;
 constexpr int GAME_MAX_PLAYER = 8;
 
