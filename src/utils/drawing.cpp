d7456dd29536a48fabde2f4960a9edc2b84c9fc3
vekxze <vekxze@proton.me>
2023-04-07 20:30:42 -0400

    fix linked 2005

---
+++ src/utils/drawing.cpp
@@ -0,0 +1,9 @@
+#include "utils/drawing.h"
+
+void DrawTextCenter(std::string const& text, int fontSize, Color color)
+{
+    const int text_width = MeasureText(text.c_str(), fontSize);
+    const int x = (GetScreenWidth() - text_width) / 2;
+    const int y = GetScreenHeight() / 2;
+    DrawText(text.c_str(), x, y, fontSize, color);
+}
--- src/utils/drawing.h
+++ src/utils/drawing.h
@@ -1,12 +1,6 @@
 #pragma once
 
-#include <string>
 #include <raylib.h>
+#include <string>
 
-void DrawTextCenter(std::string const& text, int fontSize, Color color)
-{
-    const int text_width = MeasureText(text.c_str(), fontSize);
-    const int x = (GetScreenWidth() - text_width) / 2;
-    const int y = GetScreenHeight() / 2;
-    DrawText(text.c_str(), x, y, fontSize, color);
-}
+void DrawTextCenter(std::string const& text, int fontSize, Color color);
