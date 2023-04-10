c1e143bdbdf36929e4e844cdf17afa441d69db41
vekxze <vekxze@proton.me>
2023-04-09 22:03:07 -0400

    draw timer on screen

--- src/utils/drawing.cpp
+++ src/utils/drawing.cpp
@@ -1,4 +1,5 @@
 #include "utils/drawing.h"
+#include "utils/time.h"
 
 void DrawTextCenter(std::string const& text, int fontSize, Color color)
 {
@@ -6,4 +7,13 @@ void DrawTextCenter(std::string const& text, int fontSize, Color color)
     const int x = (GetScreenWidth() - text_width) / 2;
     const int y = GetScreenHeight() / 2;
     DrawText(text.c_str(), x, y, fontSize, color);
+}
+
+void DrawTimer(long time_ms, int fontSize, Color color)
+{
+    const std::string time = time_to_string(time_ms);
+    const int text_width = MeasureText(time.c_str(), fontSize);
+    const int x = (GetScreenWidth() - text_width) - 10;
+    const int y = 10;
+    DrawText(time.c_str(), x, y, fontSize, color);
 }
--- src/utils/drawing.h
+++ src/utils/drawing.h
@@ -4,3 +4,4 @@
 #include <string>
 
 void DrawTextCenter(std::string const& text, int fontSize, Color color);
+void DrawTimer(long time_ms, int fontSize, Color color);
