b470632b8553c341be50a6be66f7a14e43fae0e3
vekxze <vekxze@proton.me>
2023-04-10 19:13:11 -0400

    update game_scene

--- src/scene/game/game_scene.cpp
+++ src/scene/game/game_scene.cpp
@@ -2,23 +2,45 @@
 
 #include "utils/constants.h"
 #include "utils/drawing.h"
+#include <raygui/raygui.h>
 
 #include <iostream>
 
-game_scene::game_scene(scene_manager& scene_manager) : scene_manager_(scene_manager){}
+game_scene::game_scene(scene_manager& scene_manager) : scene_manager_(scene_manager) {}
 
 void game_scene::update() 
 {
     const float delta_time_sec = GetFrameTime();
     current_time_ms += static_cast<long>(delta_time_sec * 1000);
+
+    player& player = game_.get_player();
+    if (player.get_action() == Action::Waiting) return; // player didn't play yet
+    // tant que le round n'est pas terminé => les bots jouent tout seul ?
+    std::cout << "he played" << std::endl;
+    player.set_action(Action::Waiting);
 }
 
 void game_scene::render()
 {
     ClearBackground(RAYWHITE);
-    DrawTextCenter("Game", 20, BLACK);
-
-    DrawTimer(current_time_ms, 20, BLACK);
 
     DrawFPS(10, 10);
+    DrawTimer(current_time_ms, 20, BLACK);
+
+    const int current_round_id = game_.get_round().get_id();
+    const int round_count = game_.get_round_count();
+    DrawRound(current_round_id, round_count, 20, BLACK);
+
+    player& player = game_.get_player();
+    if (player.get_action() == Action::Waiting)
+    {
+        if (GuiButton({ 200, 200, 100, 50 }, "Continue"))
+            player.set_action(Action::MoveForward);
+
+        if (GuiButton({ 400, 200, 100, 50 }, "Leave"))
+        {
+            player.set_action(Action::Leave);
+            player.set_exploring(false);
+        }
+    }
 }
