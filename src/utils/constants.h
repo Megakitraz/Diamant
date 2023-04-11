fa1b7dca2412347f3733a08c7e61015054d1fd7b
vekxze <vekxze@proton.me>
2023-04-10 22:34:01 -0400

    adding deck constants

--- src/utils/constants.h
+++ src/utils/constants.h
@@ -9,6 +9,11 @@ constexpr int WINDOW_TARGET_FPS = 60;
 constexpr int SPLASH_SCREEN_DURATION_MS = 500;
 
 /* INGAME CONSTANTS */
+constexpr int GAME_TREASURE_CARD = 15;
+constexpr int GAME_DANGER_CARDS = 15;
+constexpr int GAME_RELIC_CARDS = 5;
+constexpr int GAME_CARDS_COUNT = GAME_TREASURE_CARD + GAME_DANGER_CARDS + GAME_RELIC_CARDS;
+
 constexpr float GAME_BOT_WAIT_TIME_SEC = 2.0f;
 
 constexpr int GAME_MIN_PLAYER = 1;
