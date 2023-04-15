9684b6beba166be75c9b8bb5f430253571787b97
vekxze <vekxze@proton.me>
2023-04-14 22:48:42 -0400

    remove useless card_type enum

--- src/core/card/card.h
+++ src/core/card/card.h
@@ -8,30 +8,18 @@ namespace diamant {
 
     public:
 
-        enum class card_type
-        {
-            Treasure,
-            Danger,
-            Relic
-        };
-
-        card(card_type type);
+        card();
         virtual ~card();
 
-        card_type get_type() const;
         bool is_played() const;
         Texture2D const& get_texture() const;
 
-        void set_played(bool played);
-
         virtual void load_texture() = 0;
+        virtual void play();
 
     protected:
-        card_type type;
         Texture2D card_texture;
         bool played;
     };
-
-    using card_type = card::card_type;
 
 }
