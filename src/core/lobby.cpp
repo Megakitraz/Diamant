461a5e93ef15ab6cccc70cbb1d20ea05b1f70137
vekxze <vekxze@proton.me>
2023-04-15 15:38:11 -0400

    remove useless lobby class

--- src/core/lobby.cpp
+++
@@ -1,7 +0,0 @@
-#include "lobby.h"
-
-int diamant::lobby::get_player_count() const { return player_count; }
-int diamant::lobby::get_round_count() const { return round_count; }
-
-void diamant::lobby::set_player_count(int new_player_count) { player_count = new_player_count; }
-void diamant::lobby::set_round_count(int new_round_count) { round_count = new_round_count; }
