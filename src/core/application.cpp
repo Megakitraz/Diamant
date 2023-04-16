ae21ca7d80ba0e4884ea69865e91aededdaef7fa
vekxze <vekxze@proton.me>
2023-04-16 17:41:20 -0400

    remove raylib debug

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -7,6 +7,9 @@
 
 application::application()
 {
+    // Remove raylib logs
+    SetTraceLogLevel(LOG_NONE);
+
     scene_manager_.add_scene("splash_screen", std::make_unique<splash_screen_scene>(scene_manager_), true);
     scene_manager_.add_scene("title", std::make_unique<title_screen>(scene_manager_));
     scene_manager_.add_scene("game", std::make_unique<game_scene>(scene_manager_));
