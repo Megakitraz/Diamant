06def485c929e9e2e3ad36927e1f85c9b719a541
vekxze <vekxze@proton.me>
2023-04-18 12:09:26 -0400

    add diamant logo and quit button

--- src/scenes/title/title_screen.cpp
+++ src/scenes/title/title_screen.cpp
@@ -1,6 +1,7 @@
 #include "scenes/title/title_screen.h"
 #include "utils/constants.h"
 #include "utils/drawing.h"
+#include <raylib.h>
 #include <raygui/raygui.h>
 
 title_screen::title_screen(scene_manager& scene_manager) : scene_manager_(scene_manager){}
@@ -12,9 +13,14 @@ void title_screen::update() {}
 void title_screen::render()
 {
     ClearBackground(RAYWHITE);
-    if (GuiButton({ 50, 50, 150, 150 }, "New Game"))
-    {
+
+    static Texture2D diamant_logo = LoadTexture("../../assets/diamant.png");
+    static Vector2 position = { WINDOW_WIDTH / 2 - diamant_logo.width / 2, 150 };
+    DrawTextureEx(diamant_logo, position, 0.f, 1.f, WHITE);
+
+    if (GuiButton({650,400,200,50}, "New Game"))
         scene_manager_.switch_to("game");
-    }
-    DrawFPS(10, 10);
+
+    if (GuiButton({650,500,200,50}, "Quit"))
+        CloseWindow();
 }
